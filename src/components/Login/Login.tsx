1932b3a5869edc054b1f98fcb2966d1e6d59ef71
LuisFlahan4051 <luisflahan4051@gmail.com>
2020-08-18 22:04:59 -0500

    Conclui toda la parte del diseño estetico y generacion de las opciones dinamicamente, procedere a crear e implementar algun estado global con redux o el contexto de react para poder comunicarme con la BD y los componentes. Dejare como detalles para futuro las implementaciones que tenia en mente con react-spring

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,12 +1,18 @@
-import React, { useState} from 'react';
+import React, { useState , useRef } from 'react';
 import './Login.scss';
 import logo from '../../assets/media/img/miniLogoV3.svg';
 import icon from '../../assets/media/img/Down-Row.svg';
 
 
-function Login() {
-
+function Login(props: { users: React.ReactNodeArray; }) {
     const [handleSelect, setHandleSelect] = useState(false)
+    const [userValue, setUserValue] = useState("")
+
+    const inputUser = useRef(document.createElement("input"))
+    const inputPass = useRef(document.createElement("input"))
+    const inputEntry = useRef(document.createElement("button"))
+
+    inputUser.current.focus()
 
     function link() {
         console.log("Directo al link")
@@ -31,22 +37,35 @@ function Login() {
                     
                     <div className="form__select">
                         <div className="select__area" >
-                            <input className="select__input input" type="text" name="name" id="name" defaultValue={"Default"}/>
+                            <input className="select__input input" type="text"
+                            autoFocus
+                            onChange={(e: { target: { value: any } }) => setUserValue(e.target.value)}
+                            value={userValue}
+                            ref={inputUser}
+                            onKeyPress={(e: { key: any; preventDefault: () => void;}) => {
+                            if (e.key === 'Enter') {
+                                e.preventDefault();
+                                inputPass.current?.focus()
+                            }
+                            }}/>
                             <div className="select__icon" onClick={() => setHandleSelect(!handleSelect)}>
                                 <img className={handleSelect ? 'icon-motion-right' : 'icon-motion-left'} src={icon} alt="V"/>
                             </div>
                         </div>
 
                         <div className="options_area" style={handleSelect ? {} : { display:'none' } }>
-                            <div className="select__option">Opcion 1</div>
-                            <div className="select__option">Opcion 2</div>
-                            <div className="select__option">Opcion 3</div>
-                            <div className="select__option">Opcion 1</div>
-                            <div className="select__option">Opcion 2</div>
-                            <div className="select__option">Opcion 3</div>
-                            <div className="select__option">Opcion 1</div>
-                            <div className="select__option">Opcion 2</div>
-                            <div className="select__option">Opcion 3</div>
+                            {props.users.map((user) => 
+                                <div 
+                                className="select__option" 
+                                onClick={() => {
+                                    setUserValue(`${user}`)
+                                    setHandleSelect(false)
+                                }}
+                                key={user?.toString()}
+                                >
+                                {user}
+                                </div>
+                            )}
                         </div>
 
                     </div>
@@ -55,14 +74,22 @@ function Login() {
                 
                 <div className="form__fild">
                     <label className="form__label" htmlFor="">Contraseña:</label>
-                    <input className="form__password input" type="password" name="password" id="password" defaultValue={"pass"} />
+                    <input className="form__password input" 
+                    type="password" 
+                    ref={inputPass}
+                    onKeyPress={(e: { key: any; preventDefault: () => void; }) => {
+                        if (e.key === 'Enter') {
+                            e.preventDefault();
+                        inputEntry.current?.focus()
+                    }
+                    }}/>
                     <p className="form__link" onClick={link}>Olvide mi contraseña...</p>
                 </div>
                 
                 
                 <div className="form__btnArea">
+                    <button className="form__btnEntry button"  onClick={entry} ref={inputEntry}>Entrar</button>
                     <button className="form__btnCancel button" onClick={cancel}>Cancelar</button>
-                    <button className="form__btnEntry button"  onClick={entry}>Entrar</button>
                 </div>
 
             </form>
--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Login from './Login'
 
 function Index() {
+    const [user, setUser] = useState("")
+    const [pass, setPass] = useState("")
+    
+    const usersList = ["Usuario 1", "Usuario 2", "Usuario 3"]
     return (
         <div>
-            <Login />
+            <Login users={usersList} />
         </div>
     );
 }
