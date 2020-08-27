0b7e7e581933acdc2a3e7ba4344c36b43cf18564
LuisFlahan4051 <luisflahan4051@gmail.com>
2020-08-27 01:51:30 -0500

    Tengo que terminar de clasificar toda la estructura de la responsiva para proceder con el estilizado.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './App.scss'
-import Login  from './components/Login/index'
+//import Login  from './components/Login/index'
 import Responsiva from './components/Responsiva/index'
 
 function App() {
   return (
       <div className="App">
-        <Login />
+        {/* <Login /> */}
         <Responsiva />
       </div>
   );
